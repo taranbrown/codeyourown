51d7cb73d79e6e708ac2bf93cda4c132e6759216
lilyulrich1 <133797242+lilyulrich1@users.noreply.github.com>
2023-06-02 16:51:45 +0000

    Latest code

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ while choice_1 == True:
   leave == "p"
   if leave == "p":
     print ()
-    print ("You grab the pan and hit the man across the head.")
+    print ("You grab the pan and hit the man across the head, succesfully knocking him out!")
     choice_1 == False
     break
   else:
@@ -32,8 +32,8 @@ while choice_1 == True:
 print("You tie up the man with your hair. When he wakes up you order him to take you to the royal castle so that you can see the lights on your birthday!")
 input("Press any key to continue.")
 print()
-print("After a perilous journey. You finally make it to the castle. As the sun sets the lights begin to emerge and you are entranced. You look over at the man next to you. Flynn Rider.")
-smooch = input("Do you give this man a special thank you? enter y for yes or n for no").lower()
+print("After a perilous journey. You finally make it to the castle. As the sun sets, the lights begin to emerge and you are entranced. Your dreams have finally come true! You look over at the man next to you. Flynn Rider.")
+smooch = input("Do you give this man a SPECIAL thank you? enter y for yes or n for no").lower()
 if smooch == "y":
   print("You say thanks...")
 elif smooch == "n":
@@ -42,18 +42,18 @@ else:
   print("That was not an option. You probably got confused because you were so nervous around Flynn so we'll help you out. You said thanks...")
 input ("Press any key to continue.")
 print()
-print("""The night ends and you are very happy BUT just as you are about to leave you hear a woman shout. IT'S MOTHER GOTHEL! OH NO!
-She is very disappointed to see you with this strange boy and immediately takes you home.""")
+print("""The night ends and you are very happy...but just as you are about to leave you hear a woman shout, "RAPUNZEL!!!!!!!" IT'S MOTHER GOTHEL! OH NO!
+She is very disappointed to see you with this strange boy and immediately takes you back to the tower.""")
 input ("Press any key to continue.")
 print ()
 ####CHoice 3 ####
-print("You are back at the tower with your mom and you seem to find yourself missing FLynn. Just then he enters the tower! He fights off your evil mother and grabs a piece of glass. He is going to cut your hair")
+print("You are back at the tower with your mom and find yourself missing FLynn. Just then he enters the tower! Just as he is about to help you escape, Mother Gothel jumps out from the corner!! He fights off your evil mother and grabs a piece of glass. He is about to cut your hair...")
 cut_hair = input("Do you let him cut your hair? Enter y for yes or n for no").lower()
 if cut_hair == "y":
   print("You let him cut your hair. It's SUPER SHORT and it turns dark brown. It is no longer golden :( Fortunately with you hair gone, your mother dies. Yay! You get to live happily ever after with Flynn.")
   
 if cut_hair == "n":
-  print("You don't let Flynn cut your hair. He gets distracted and your mom kills him. He's dead and you remain a prisoner for the rest of your life.")
+  print("You don't let Flynn cut your hair. He gets distracted and your mom kills him. He's dead and you remain a prisoner for the rest of your life...")
 else:
   print("That was not an option. You don't let him cut your hair so your mom kills him and keeps you prisoner")
 
