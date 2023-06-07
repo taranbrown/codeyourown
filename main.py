c895c2e68f2ced98c2602a37b49e9613ef9eb518
taranbrown <133796987+taranbrown@users.noreply.github.com>
2023-06-07 15:44:34 +0000

    final

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
+
 import saved
-
-
-user = saved.load()
-if user is None:
-  user = input ("Please enter the username you would like to use.")
-  saved.save(user)
-else:
-  print ("Welcome back,", user)
-  
+ 
 
 input("You have entered the Rapunzel story. Press any key to continue")
 import choice1
